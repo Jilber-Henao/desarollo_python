acc03d4c13af5f7b133478614c6140c5e207298c
Jilber Henao <jilberhenao@gmail.com>
2025-05-03 11:53:07 -0500

    cree un archivo nuevo llamdo listas.py

--- ejercicio5.py
+++ ejercicio5.py
@@ -3,3 +3,6 @@ lado_1 = int(input("ingrese el largor del rectangulo"))
 lado_2 = int(input("ingrese el anchor del rectangulo"))
 perimetro = (lado_1 + lado_2)*2
 print(f"el perimetro del cuadrado es de {perimetro}")
+
+
+"""esto es un cambio en la rama mi rama"""
